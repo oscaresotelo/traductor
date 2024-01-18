f2bc38ef0a45e0ca9c2008b58ff100741797aa88
oscarsotelo <oscaresotelo@gmail.com>
2024-01-18 12:41:10 -0300

    12

--- voz.py
+++ voz.py
@@ -131,7 +131,7 @@ target_language = st.selectbox("Seleccione el idioma a traducir:", languages)
 translate = st.button('Traducir')
 if translate:
     translator = Translator()
-    out = translator.translate(source_text,dest='en')
+    out = translator.translate(source_text,dest=languages)
     st.write(out.text)
         
                                
