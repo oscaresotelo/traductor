841fe406f9f2ae4394523c91cdd398e29216f080
oscarsotelo <oscaresotelo@gmail.com>
2024-01-18 12:29:22 -0300

    octava

--- vervoces.py
+++ vervoces.py
@@ -1,6 +1,11 @@
-import pyttsx3
+# import pyttsx3
 
-engine = pyttsx3.init()
+# engine = pyttsx3.init()
 
-for voice in engine.getProperty('voices'):
-    print(voice)
+# for voice in engine.getProperty('voices'):
+#     print(voice)
+from googletrans import Translator
+
+translator = Translator()
+traduccion = translator.translate("hola como estas", dest= "en")
+print(traduccion.text)
