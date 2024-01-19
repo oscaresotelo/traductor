6c9439a0e194d20096eeab7f106cf9f5dc94fd54
oscarsotelo <oscaresotelo@gmail.com>
2024-01-19 15:18:21 -0300

    ultimadelviernes

--- voz.py
+++ voz.py
@@ -1,65 +1,4 @@
-# # import streamlit as st
-# # from bokeh.models.widgets import Button
-# # from bokeh.models import CustomJS
-# # from streamlit_bokeh_events import streamlit_bokeh_events
-# # from googletrans import Translator
 
-# # from languages import *
-# # import pyttsx3
-
-# # speak = pyttsx3.init()
-# # voices = speak.getProperty('voices')
-# # voice_id = 'english'
-# # speak.setProperty('voice', voice_id)
-
-# # st.title("Traductor Universal.oscar inc.")
-
-# # stt_button = Button(label="Speak", width=100)
-
-# # stt_button.js_on_event("button_click", CustomJS(code="""
-# #     var recognition = new webkitSpeechRecognition();
-# #     recognition.continuous = true;
-# #     recognition.interimResults = true;
- 
-# #     recognition.onresult = function (e) {
-# #         var value = "";
-# #         for (var i = e.resultIndex; i < e.results.length; ++i) {
-# #             if (e.results[i].isFinal) {
-# #                 value += e.results[i][0].transcript;
-# #             }
-# #         }
-# #         if ( value != "") {
-# #             document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-# #         }
-# #     }
-# #     recognition.start();
-# #     """))
-
-# # result = streamlit_bokeh_events(
-# #     stt_button,
-# #     events="GET_TEXT",
-# #     key="listen",
-# #     refresh_on_update=False,
-# #     override_height=75,
-# #     debounce_time=0)
-
-
-# # # target_language = st.selectbox("Seleccione el idioma a traducir:", languages)
-# # # target_language = st.selectbox("Seleccione el idioma a traducir:", "English")
-# # if result:
-# #     if "GET_TEXT" in result:
-# #         source_text = result.get("GET_TEXT")
-# #         st.write(source_text)
-
-        
-# #         # translate = st.button('Traducir')
-# #         # if translate:
-# #         translator = Translator()
-# #         out = translator.translate(source_text,dest="English")
-# #         st.write(out.text)
-# #         salida = out.text
-# #         speak.say(salida)
-# #         speak.runAndWait()
 
 import streamlit as st
 from bokeh.models.widgets import Button
@@ -71,11 +10,37 @@ import pyttsx3
 from gtts import gTTS
 from io import BytesIO
 sound_file = BytesIO()
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+                     .container {
+                display: flex;
+            }
+            .logo-text {
+                font-weight:700 !important;
+                font-size:30px !important;
+                color: black !important;
+                padding-top: 50px !important;
+            }
+            .logo-img {
+                float:right;
+            }
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+def local_css(file_name):
+        with open(file_name) as f:
+            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+local_css("estilos.css")
 # speak = pyttsx3.init()
 # voices = speak.getProperty('voices')
 # voice_id = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-MX_SABINA_11.0'
 # speak.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-MX_SABINA_11.0')
 translator = Translator()
+st.image("oscar.jpg")
 st.title("Traductor Universal.oscar inc.")
 
 stt_button = Button(label="Presiona para Hablar", width=100)
